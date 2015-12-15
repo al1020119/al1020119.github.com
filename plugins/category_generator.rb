ccef6450c7e331adfc6f8ba560cd6678a0580564
iCocos <al10201119@163.com>
2015-12-15 11:24:16 +0800

    Some comment here.

--- plugins/category_generator.rb
+++ plugins/category_generator.rb
@@ -22,6 +22,24 @@
 require 'stringex'
 
 module Jekyll
+    
+#        class CategoryListTag < Liquid::Tag
+#            def render(context)
+#                html = ""
+#                categories = context.registers[:site].categories.keys
+#                categories.sort.each do |category|
+#                    posts_in_category = context.registers[:site].categories[category].size
+#                    category_dir = context.registers[:site].config['category_dir']
+#                    category_url = File.join(category_dir, category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase)
+#                    html << "<li class='category'><a href='/#{category_url}/'>#{category} (#{posts_in_category})</a></li>\n"
+#                end
+#                html
+#            end
+#        end
+#    end
+#
+#    Liquid::Template.register_tag('category_list', Jekyll::CategoryListTag)
+
 
   # The CategoryIndex class creates a single category page for the specified category.
   class CategoryIndex < Page
@@ -49,6 +67,8 @@ module Jekyll
     end
 
   end
+
+
 
   # The CategoryFeed class creates an Atom feed for the specified category.
   class CategoryFeed < Page
